04682a826b73521755ff8377803643fdb512be8b
RileyK19 <rileykoo19@gmail.com>
2024-02-23 11:50:25 -0600

    Initial Commit

---
+++ testfeb23/testfeb23App.swift
@@ -0,0 +1,17 @@
+//
+//  testfeb23App.swift
+//  testfeb23
+//
+//  Created by Riley Koo on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct testfeb23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
